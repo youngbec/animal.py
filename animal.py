71eeb5d351c0bbb0db407c8750ae4a8bf838ac22
topher346 <christopher.l.youngberg@hotmail.com>
2024-04-02 21:43:35 -0700

    Initial commit

---
+++ animal.py
@@ -0,0 +1,7 @@
+# Author: Christopher Youngberg
+# GitHub username: youngbec
+# Date: 04/02/2024
+# Description: Asks the user for their favorite animal and then prints out that that is their favorite animal.
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
